bd892fd52d2c5537f2cbc4705c57001208989f8b
Vijan Pavlin <pavlinvijan107@gmail.com>
2025-11-12 19:09:59 +0100

    optimal solution attempt copy pster

---
+++ euler10.py
@@ -0,0 +1,14 @@
+
+def sieve_of_eratosthenes(limit):
+    is_prime = [True] * limit
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(limit**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, limit, i):
+                is_prime[j] = False
+    return sum(i for i in range(limit) if is_prime[i])
+
+result = sieve_of_eratosthenes(200000)
+print(result)
+
+

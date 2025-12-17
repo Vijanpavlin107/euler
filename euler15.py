f626b944b12e2d1d6e7e825634973d94bf46d4f8
Vijan Pavlin <pavlinvijan107@gmail.com>
2025-12-17 21:27:08 +0100

    gre skozi samo 1 cikl, ko zadene x=y=20 se ustavi in breakne vn in ne preveri vseh drugih moznosti??

--- euler15.py
+++ euler15.py
@@ -2,14 +2,29 @@
 #20x20 grid
 #lahko se premikam le desno in dol
 #stevilo moznih poti
-
 #na vsakem krizicu imam lahko 2 ali pa 1 moznost (ce sem ze na desnem ali spodnjem robu) ==> 
 #moram simulirati?
-#poskusim
+#poskusim simulirat tako da najprej zacnem v (0,0) in preverim koliko je moznih premikov in nato rekurzivno za vsak mozni premik nazaj poklicem funkcijo
+#break se zgodi ko pridem v (20, 20) takrat pristejem eno pot
 
-moznosti = ['desno', 'dol']
-poz = (0,0)
-num = 0
 
-while poz != (20, 20):
+dimenzija = 20
+
+def possible_moves(x, y):
+    if x == dimenzija and y == dimenzija:
+        print("hit")
+        return 1
     
+    poti = 0
+    if x < dimenzija:
+        print('X')
+        poti += possible_moves(x+1, y)
+    elif y < dimenzija:
+        print("Y")
+        poti += possible_moves(x,y+1)
+    
+    return poti
+
+total = possible_moves(0,0)
+print(total)
+

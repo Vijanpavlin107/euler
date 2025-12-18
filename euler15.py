6d7648cb2a7a835cb9e8728947887c7bd86b83f5
Vijan Pavlin <pavlinvijan107@gmail.com>
2025-12-18 21:49:28 +0100

    first solution was slow second solution with hint from chat still not good

--- euler15.py
+++ euler15.py
@@ -8,23 +8,39 @@
 #break se zgodi ko pridem v (20, 20) takrat pristejem eno pot
 
 
-dimenzija = 20
+# dimenzija = 20
 
-def possible_moves(x, y):
-    if x == dimenzija and y == dimenzija:
-        print("hit")
-        return 1
-    
-    poti = 0
-    if x < dimenzija:
-        print('X')
-        poti += possible_moves(x+1, y)
-    elif y < dimenzija:
-        print("Y")
-        poti += possible_moves(x,y+1)
-    
-    return poti
+# def possible_moves(x, y):
+#     if x == dimenzija and y == dimenzija:
+#         return 1
+#     else:
 
-total = possible_moves(0,0)
-print(total)
+#         poti = 0
+#         if x < dimenzija:
+#             poti += possible_moves(x+1, y)
+#         if y < dimenzija:
+#             poti += possible_moves(x,y+1)
+        
+#         return poti
 
+
+# total = possible_moves(1,1)
+# print(total)
+#zgornja koda je veliko pre pocasna
+
+#namig: problem se preveda na iskanje stevila moznosti razporeditve n elementov na 2n mest
+#to je zato ker je v vsaki poti ocitno 2n premikov n izmed katerih je dol in n desno
+#sledi da se da vse poti zapisati kot pozicije premikov dol / desno izmed 2n skupnih premikov
+#to se izracuna: 2n! / (n! * n!)
+
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+def stevilo_moznih_poti(dim):
+    izraz = factorial(dim * 2) / (factorial(dim)^2)
+    return izraz
+
+print(stevilo_moznih_poti(20))

35bfa3f6ceabce048c6fa023b81d2b2fbfc96f6a
Vijan Pavlin <pavlinvijan107@gmail.com>
2025-12-20 22:11:56 +0100

    easy solution

--- euler16.py
+++ euler16.py
@@ -1,1 +1,26 @@
 #the sum of the digits of 2^1000
+
+#brute force
+
+# def sum_digits(n):
+#     sestevek = 0
+#     while n != 0:
+#         digit = n % 10
+#         sestevek += digit
+#         n = (n - digit) / 10
+#         print(sestevek, digit, n)
+    
+#     return sestevek
+
+
+# print(sum_digits(2**1000))
+
+def sum_digits(n):
+    sestevek = 0
+    while n != 0:
+        digit = n % 10
+        sestevek += digit
+        n = n // 10
+    return sestevek
+
+print(sum_digits(2**1000))

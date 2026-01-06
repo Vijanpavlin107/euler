5de412fad6049f32931cb548ca07a81f3a68a96c
Vijan Pavlin <pavlinvijan107@gmail.com>
2026-01-06 21:17:23 +0100

    started problem 17

---
+++ euler17.py
@@ -0,0 +1,8 @@
+#number of letters (not counting hyphens or spaces) in all the numbers (writen with letters) from 1 to 1000
+
+#every number is a compilatin of stotice, desetice and enice: a-hundread and b-ty c
+#hundreds are just ones but with 'hundread' at the end so plus 8 chars
+
+relatins_ones = {1:3, 2:3, 3:5, 4:4, 5:4, 6:3, 7:5, 8:5, 9:4, 0:0}
+relations_tens = {1:}
+print(relations_hundreads[1])

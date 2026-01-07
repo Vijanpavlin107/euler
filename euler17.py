3989c58882af625128b5813924995c43a6afda03
Vijan Pavlin <pavlinvijan107@gmail.com>
2026-01-07 21:08:11 +0100

    put all relatoins in a list of dics so i can call by iteration (enice, desetice, stotice(le enice + 3))

--- euler17.py
+++ euler17.py
@@ -3,6 +3,46 @@
 #every number is a compilatin of stotice, desetice and enice: a-hundread and b-ty c
 #hundreds are just ones but with 'hundread' at the end so plus 8 chars
 
-relatins_ones = {1:3, 2:3, 3:5, 4:4, 5:4, 6:3, 7:5, 8:5, 9:4, 0:0}
-relations_tens = {1:}
-print(relations_hundreads[1])
+import errno
+
+
+relations_ones = {1:3, 2:3, 3:5, 4:4, 5:4, 6:3, 7:5, 8:5, 9:4, 0:0}
+
+relations_tens = {
+    0: 0,
+    1: 3,   # ten
+    2: 6,   # twenty
+    3: 6,   # thirty
+    4: 5,   # forty
+    5: 5,   # fifty
+    6: 5,   # sixty
+    7: 7,   # seventy
+    8: 6,   # eighty
+    9: 6    # ninety
+}
+
+# hundreds = ones + "hundred"
+relations_hundreds = {
+    k: v + 7 for k, v in relations_ones.items()
+}
+
+relacije = [
+    relations_ones,
+    relations_tens,
+    relations_hundreds
+]
+
+#for every ten there are 10 options totaling whatever amount of chars there are untill the ones times 10 plus the sum off all the options
+sestevek_enic = sum(relations_ones.values())
+
+#sestavim funkcijo ki za dano stevilo presteje stevilo znakov 
+def stevilo_znakov(n):
+    if n > 1000:
+        raise ValueError("n is too large")
+    if n == 1000:
+        return 8
+    
+    l = 1
+    while l < 3:
+        
+    
